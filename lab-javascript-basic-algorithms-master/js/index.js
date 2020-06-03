2fee8a9dd5e35c1d58652c257d864c620ad81fb5
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-06-03 10:26:53 +0200

    instructions

--- lab-javascript-basic-algorithms-master/js/index.js
+++ lab-javascript-basic-algorithms-master/js/index.js
@@ -1,7 +1,55 @@
+// ## Instructions
+
+// ### Iteration 1: Names and Input
+
+// 	1.1 Create a variable `hacker1` with the driver's name.
+
+// 	1.2 Print `"The driver's name is XXXX"`.
+
+//   	1.3 Create a variable `hacker2` with the navigator's name.
+
+//   	1.4 Print `"The navigator's name is YYYY"`.
+
+// ### Iteration 2: Conditionals
+//   2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
+//   <br>
+// 	  - `The driver has the longest name, it has XX characters.` or <br>
+// 	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
+// 	  - `Wow, you both have equally long names, XX characters!`.
+
+// ### Iteration 3: Loops
+//   3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+//   i.e. `"J O H N"`
+
+//   3.2 Print all the characters of the navigator's name, in reverse order.
+//   i.e. `"nhoJ"`
+
+//   3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+//     - `The driver's name goes first.` <br>
+//     - `Yo, the navigator goes first definitely.` <br>
+//     - `What?! You both have the same name?`
+
+// ### Bonus Time!
+
+// #### Bonus 1:
+// Go to [lorem ipsum generator](http://www.lipsum.com/) and:
+//   - Generate 3 paragraphs. Store the text in a variable type of string.
+//   - Make your program count the number of words in the string.
+//   - Make your program count the number of times the Latin word [`et`](https://en.wiktionary.org/wiki/et#Latin) appears.
+
+// #### Bonus 2:
+// Create a new variable `phraseToCheck` and have it contain some string value. Write a code that will check if the value we assigned to this variable is a [Palindrome](https://en.wikipedia.org/wiki/Palindrome). Here are some examples of palindromes:
+//   - "A man, a plan, a canal, Panama!"
+//   - "Amor, Roma"
+//   - "race car"
+//   - "stack cats"
+//   - "step on no pets"
+//   - "taco cat"
+//   - "put it up"
+//   - "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+
 // Iteration 1: Names and Input
 
-
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
