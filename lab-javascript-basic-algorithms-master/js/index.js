131a39edbfc0bd08ab6d742ef152b8a34c424e8c
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-06-03 11:44:16 +0200

    exercice done

--- lab-javascript-basic-algorithms-master/js/index.js
+++ lab-javascript-basic-algorithms-master/js/index.js
@@ -1,55 +1,70 @@
-// ## Instructions
-
-// ### Iteration 1: Names and Input
-
-// 	1.1 Create a variable `hacker1` with the driver's name.
-
-// 	1.2 Print `"The driver's name is XXXX"`.
-
-//   	1.3 Create a variable `hacker2` with the navigator's name.
-
-//   	1.4 Print `"The navigator's name is YYYY"`.
-
-// ### Iteration 2: Conditionals
-//   2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
-//   <br>
-// 	  - `The driver has the longest name, it has XX characters.` or <br>
-// 	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
-// 	  - `Wow, you both have equally long names, XX characters!`.
-
-// ### Iteration 3: Loops
-//   3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
-//   i.e. `"J O H N"`
-
-//   3.2 Print all the characters of the navigator's name, in reverse order.
-//   i.e. `"nhoJ"`
-
-//   3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
-//     - `The driver's name goes first.` <br>
-//     - `Yo, the navigator goes first definitely.` <br>
-//     - `What?! You both have the same name?`
-
-// ### Bonus Time!
-
-// #### Bonus 1:
-// Go to [lorem ipsum generator](http://www.lipsum.com/) and:
-//   - Generate 3 paragraphs. Store the text in a variable type of string.
-//   - Make your program count the number of words in the string.
-//   - Make your program count the number of times the Latin word [`et`](https://en.wiktionary.org/wiki/et#Latin) appears.
-
-// #### Bonus 2:
-// Create a new variable `phraseToCheck` and have it contain some string value. Write a code that will check if the value we assigned to this variable is a [Palindrome](https://en.wikipedia.org/wiki/Palindrome). Here are some examples of palindromes:
-//   - "A man, a plan, a canal, Panama!"
-//   - "Amor, Roma"
-//   - "race car"
-//   - "stack cats"
-//   - "step on no pets"
-//   - "taco cat"
-//   - "put it up"
-//   - "Was it a car or a cat I saw?" and "No 'x' in Nixon".
-
 // Iteration 1: Names and Input
+const hacker1 = "Vladimir";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Marie";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker2.length > hacker1.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.  `
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker2.length} characters!  `
+  );
+}
+
 // Iteration 3: Loops
+
+console.log(`"` + hacker1.split("").join(" ").toUpperCase() + `"`);
+console.log(`"` + hacker2.split("").reverse("").join("") + `"`);
+
+var result = hacker2.localeCompare(hacker1);
+
+console.log(result);
+
+if (result < 0) {
+  console.log("The driver's name goes first.");
+} else if (result > 0) {
+  console.log("Yo, the navigator goes first definitely.");
+} else if (result === 0) {
+  console.log("What?! You both have the same name?");
+}
+
+// Bonus 1:
+
+let loremIpsum =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque tempus. Morbi blandit id orci sed luctus. Donec eu sem non purus feugiat aliquam. Etiam vel dictum diam. Phasellus non suscipit lacus, vitae pulvinar quam. Duis fermentum eu lacus id sollicitudin. Cras eget turpis ullamcorper tellus sollicitudin pharetra. Nulla vitae sem venenatis, eleifend sapien ut, efficitur nulla. Nullam convallis justo sit amet velit luctus molestie ut et libero. Nam iaculis, dui nec feugiat dapibus, sapien diam lacinia neque, ut sollicitudin tellus sapien a mauris. Maecenas tempor feugiat ante, sed molestie ex porta id. Nulla ac odio vitae quam mollis finibus. Nulla auctor sagittis vehicula. Aliquam urna enim, tincidunt id tempor et, venenatis sit amet ipsum. Phasellus libero dolor, tincidunt ut nisl in, ornare facilisis lacus. Fusce viverra lorem eu ultricies pretium.Nunc tempor sit amet felis eget ultricies. Nullam dapibus magna eget mattis pretium. Nullam sed egestas sapien. Nam auctor ornare libero, in rhoncus nulla sodales vel. Nullam suscipit iaculis arcu vitae rutrum. Duis euismod fringilla mauris, at ultricies mi lobortis sit amet. Etiam augue diam, imperdiet quis metus et, viverra pulvinar justo. Nunc eget luctus felis, varius viverra neque.Donec eleifend laoreet convallis. Proin sit amet libero urna. Vestibulum placerat, risus vel ultrices semper, mi magna aliquam ligula, a varius ex eros rutrum mauris. Curabitur suscipit euismod nibh, vitae fringilla sem interdum sed. Sed vel faucibus massa. Sed pulvinar augue vitae ultricies hendrerit. Nulla in porttitor neque. Suspendisse scelerisque maximus purus eget vestibulum. Nullam eu risus quam. Morbi sodales eget ex non feugiat. Duis at libero magna. Aenean sit amet risus tellus. Donec sollicitudin sit amet metus eu sollicitudin. Sed sit amet scelerisque enim. Aenean semper consequat velit, et efficitur est volutpat quis.";
+
+// console.log(loremIpsum)
+
+const countWords = (string) => {
+  console.log(string.trim().split(/\s+/).length);
+};
+
+var word_matches = loremIpsum.match(/et/gi).length;
+
+countWords(loremIpsum);
+console.log(word_matches);
+
+// Bonus 2:
+
+function palindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, "");
+  var len = str.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(palindrome("radar"));
