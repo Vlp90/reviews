add5915262013ede967a00f3f4ea03ebb50398bc
Vlp90 <vladimir.leloup@gmail.com>
2020-06-10 14:22:04 +0200

    logic done

--- lab-javascript-chronometer-master/javascript/chronometer.js
+++ lab-javascript-chronometer-master/javascript/chronometer.js
@@ -1,26 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime = this.currentTime + 1;
+      if (callback) {
+        callback();
+      }
+    }, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
+
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(number) {
+    if (number < 10) {
+      return `0${number}`;
+    } else {
+      return number.toString();
+    }
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
+
   splitClick() {
-    // ... your code goes here
+    const formatedMinutes = this.twoDigitsNumber(this.getMinutes());
+    const formatedSeconds = this.twoDigitsNumber(this.getSeconds());
+
+   return `${formatedMinutes}:${formatedSeconds}`;
+    
   }
 }
+
+const test = new Chronometer();
+console.log(test);
