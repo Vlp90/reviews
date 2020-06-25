747bdeb8c6eeeb0f61e90d9f640976f54709f9ae
Vlp90 <vladimir.leloup@gmail.com>
2020-06-25 15:40:22 +0200

    exercice done

--- lab-mongoose-recipes-master/index.js
+++ lab-mongoose-recipes-master/index.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
@@ -22,6 +23,61 @@ mongoose
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });
+
+//  CREATE ONE RECIPE
+Recipe.create({
+  title: "La recette de Vlad",
+  level: "Easy Peasy",
+  ingredients: ["Tomates", "Concombre", "Carottes"],
+  cuisine: "Super simple",
+})
+  .then((newRecipe) => console.log("La nouvelle recette est :", newRecipe))
+  .catch((err) => console.log("Error", err))
+
+  // SEVERAL FROM FILE JSON
+  .then(() =>
+    Recipe.insertMany(data)
+      .then((dbRes) => console.log(`Il y a ${dbRes.length} recettes `))
+      .then(() =>
+        data.forEach((recipe) =>
+          console.log(`La recette '${recipe.title}' a été ajoutée `)
+        )
+      )
+      .catch((error) => console.log(error))
+  )
+
+  .then(() =>
+    // FIND AND UPDATE RECIPE
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    )
+      .then((newDuration) =>
+        console.log(
+          `La nouvelle durée de preparatione de ${newDuration.title} est `,
+          newDuration.duration
+        )
+      )
+      .catch((error) => console.log(error))
+  )
+
+  .then(() =>
+    Recipe.deleteOne({ name: "Carrot Cake" })
+      .then(
+        console.log(
+          `Carrot Cake a été supprimé...`,
+          `Nous avons maintenant ${data.length} recettes`
+        )
+      )
+      .catch((error) => console.log(error))
+  )
+  .then(() =>
+    mongoose.connection.close().then(console.log("Extinction BDD..."))
+  )
+  .catch((error) => {
+    console.log(error);
   });
