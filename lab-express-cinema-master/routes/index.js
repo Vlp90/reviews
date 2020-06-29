e7f14fc1b72a6757732da5e74fb668dec7d9f3fd
Vlp90 <vladimir.leloup@gmail.com>
2020-06-29 12:01:34 +0200

    exercice done

--- lab-express-cinema-master/routes/index.js
+++ lab-express-cinema-master/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Movies = require("../models/movieModel");
+const { db } = require("../models/movieModel");
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
@@ -8,16 +9,24 @@ router.get("/", (req, res, next) => res.render("index"));
 router.get("/movies", (req, res, next) => {
   Movies.find()
     .then((dbRes) => {
-        res.render('movies', {
-            movies: dbRes
-        })
-    //   console.log(dbRes);
+      res.render("movies", {
+        movies: dbRes,
+      });
+      //   console.log(dbRes);
     })
     .catch((err) => {
-      console.log(err);
+      // console.log(err);
     });
 });
 
-
+router.get("/movie/:id", (req, res) => {
+  Movies.findById(req.params.id)
+    .then((debResponse) => {
+      res.render("movieID", {
+        movieID: debResponse,
+      });
+    })
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
