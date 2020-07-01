78b735ba7a4fe811c97dc43dcd593ec0f7fa686e
Vlp90 <vladimir.leloup@gmail.com>
2020-07-01 12:45:15 +0200

    setup ok

--- lab-express-spotify-master/app.js
+++ lab-express-spotify-master/app.js
@@ -1,18 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/", (req, res) => res.render("index.hbs"));
+
+app.listen(5000, () =>
+  console.log("My Spotify project running on port 5000 🎧 🥁 🎸 🔊")
+);
