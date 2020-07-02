ebf922356e7c517893c42cfc3aa4fd446926000b
Vlp90 <vladimir.leloup@gmail.com>
2020-07-02 15:26:50 +0200

    log in ok

--- lab-express-basic-auth-master/routes/auth.js
+++ lab-express-basic-auth-master/routes/auth.js
@@ -67,4 +67,44 @@ router.get("/login", (req, res, next) => {
   res.render("./auth/login");
 });
 
+
+router.post('/login', (req, res, next) => {
+	const theUsername = req.body.username;
+	const thePassword = req.body.password;
+
+	if (theUsername === '' || thePassword === '') {
+		res.render('auth/login', {
+			errorMessage: 'Please enter both, username and password to sign up.'
+		});
+		return;
+	}
+
+	User.findOne({
+			username: theUsername
+		})
+		.then((user) => {
+			if (!user) {
+                console.log(user)
+				res.render('auth/login', {
+					errorMessage: "The username doesn't exist."
+				});
+				return;
+			}
+			if (bcrypt.compareSync(thePassword, user.password)) {
+				// Save the login in the session!
+				req.session.currentUser = user;
+				res.redirect('main');
+			} else {
+				res.render('auth/login', {
+					errorMessage: 'Incorrect password'
+				});
+			}
+		})
+		.catch((error) => {
+			next(error);
+		});
+});
+
+
+
 module.exports = router;
