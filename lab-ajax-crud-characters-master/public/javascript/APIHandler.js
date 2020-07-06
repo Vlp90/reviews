d0e5784f1ea511af2a1fd42208a886620365c865
Vlp90 <vladimir.leloup@gmail.com>
2020-07-06 17:47:17 +0200

    fetch all done

--- lab-ajax-crud-characters-master/public/javascript/APIHandler.js
+++ lab-ajax-crud-characters-master/public/javascript/APIHandler.js
@@ -1,25 +1,26 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    // this.BASE_URL = baseUrl;
+    this.service = axios.create({ baseURL: baseUrl });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.service.get("/characters");
   }
 
-  getOneRegister () {
+  // getOneRegister(id) {
+  //   return this.service.get("/characters/" + id);
+  // }
 
-  }
+  // createOneRegister(data) {
+  //   return this.service.post("/characters", data);
+  // }
 
-  createOneRegister () {
+  // updateOneRegister(id, data) {
+  //   return this.service.patch("/characters/" + id, data);
+  // }
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  // deleteOneRegister(id) {
+  //   return this.service.delete("/characters/" + id);
+  // }
 }
