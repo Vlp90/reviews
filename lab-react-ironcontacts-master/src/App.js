78b401681108863e1e0e147a8372a10afe127b46
Vlp90 <vladimir.leloup@gmail.com>
2020-07-15 11:25:10 +0200

    table done, with import contacts

--- lab-react-ironcontacts-master/src/App.js
+++ lab-react-ironcontacts-master/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ContactsList from './components/ContactsList';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contactData: contacts.slice(0, 10),
+  };
+
+  //  addRandomContact = () => {
+  //     let min = 5;
+  //     let max = contacts.length;
+  //     const randomIndex = Math.floor(Math.random() * (max - min)) + min;
+  //     const copy = [...this.state.contactData];
+  //     const newContact = contacts[randomIndex];
+  //     copy.push(newContact);
+  //     this.setState({ contactData: copy });
+  //   };
+
+  //   sortByName = () => {
+  //     this.setState({
+  //       contactData: this.state.contactData.slice().sort((a, b) => {
+  //         if (a.name.toLowerCase() < b.name.toLowerCase()) {
+  //           return -1;
+  //         } else {
+  //           return 1;
+  //         }
+  //       })
+  //     });
+  //   };
+
+  //   sortByPopularity = () => {
+  //     this.setState({
+  //       contactData: this.state.contactData.slice().sort((a, b) => {
+  //         return b.popularity - a.popularity;
+  //       })
+  //     });
+  //   };
+
+  //   deleteContact = index => {
+  //     const withoutContact = [...this.state.contactData];
+  //     withoutContact.splice(index, 1);
+
+  //     this.setState({
+  //       contactData: withoutContact
+  //     });
+  //   };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        {/* <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button> */}
+
+        <div className="App">
+          <ContactsList
+            // deleteContact={this.deleteContact}
+            contacts={this.state.contactData}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
